e3d1a41fe03dc9428638cdebfdf2b2f29c7f31f9
Madhur Garg <madhurgarg96@gmail.com>
2019-04-16 07:20:43 +0530

    removed extra directory

---
+++ README.md
@@ -0,0 +1,63 @@
+# Project Title
+
+Identification of stolen vehicles and analyzing the same.
+
+## Getting Started
+
+The follwing instructions will tell you about what we are proposing in the project, how we are implementing it and the results we are getting from the same.
+
+### About the project
+
+In this project we are analyzing the data of stolen vehicles and finding interesting results from the same.
+
+### Prerequisites
+
+You need to install the following python libraries for the provided codes to work :
+```
+csv
+pandas
+matplotlib 
+datetime
+operator
+subprocess
+json
+gmaps
+datetime
+requests
+openalpr
+```
+## Refer:
+For Vehicle Registration Identification
+http://doc.openalpr.com/cloud_api.html
+
+For Google Map Heatmap
+https://github.com/pbugnion/gmaps
+
+### Implementation
+
+Run the jupyter notebook ipython file as :
+
+```
+1. Open jupyter notebook
+2. run the file "data.ipynb"
+```
+## What we are doing?
+
+We are scrapping data of stolen vehicles from the website "http://zipnet.in/index.php?page=stolen_vehicles".
+After scrapping the data we are analyzing the data to find some results which may indicate something.
+Analyzing the data includes finding Heatmaps, Wordcloud, and implementing different algorithms on the data.
+
+## Result
+
+## Authors
+
+* **Saksham Gupta** - [shucon](https://github.com/shucon)
+* **Kushagra Nagori** - [kushagra189](https://github.com/kushagra189)
+* **Kalpit Pokra** - [kalpit4088](https://github.com/kalpit4088)
+* **Madhur Garg** - [madhur96](https://github.com/madhur96)
+* **Vikrant Deshmukh** - [vikrant1697](https://github.com/vikrant1697)
+
+## Acknowledgments
+
+* This project is a part of BDP (Big data and policing) course under "Prof. Ponnurangam Kumaraguru".
+
---
+++ web-app/app.py
@@ -0,0 +1,85 @@
+import os
+import base64
+import json
+import requests
+import pandas as pd
+from PIL import Image
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_folder="uploads")
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+colors = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA", "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1", "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+@app.route('/')
+def page():
+    return render_template('index.html')
+
+@app.route('/heatmap')
+def heatmap():
+    return render_template('map.html')
+
+@app.route('/festival')
+def chart3():
+    label = ['Navratri', 'Diwali', 'New Year', 'Normal Days']
+    value = [141, 124, 144, 115]
+    return render_template('festive.html', title='Festival Analysis', max=145, labels=label, values=value)
+
+@app.route('/day_analysis')
+def chart():
+    label = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    value = [1999, 3577, 3401, 3284, 2968, 3142, 2526]
+    return render_template('day.html', title='Day Analysis Report', max=4000, labels=label, values=value,set=zip(value, label, colors))
+
+@app.route('/vehicle_type')
+def chart1():
+    label = ['m-cycle/scooter', 'motor cycle', 'motor car', 'car', 'scooter', 'e-rickshaw(p)', 'goods carrier', 'scooty', 'motor cab', 'three wheeler']
+    value = [7823, 4007, 2917, 1891, 1143, 653, 250, 242, 199, 184]
+    return render_template('vehicle.html', title='Count of stolen vehicles', max=8000, labels=label, values=value)
+
+@app.route('/date_analysis')
+def chart2():
+    label = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    value = [648, 653, 658, 735, 871, 677, 777, 733, 706, 721, 746, 658, 553, 627, 555, 547, 585, 903, 749, 628, 667, 832, 689, 725, 820, 612, 590, 606, 688, 555, 383]
+    return render_template('date.html', title='Date wise Analysis', max=950, labels=label, values=value)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    name = file.filename
+    name = name.split('.')
+    if name[1]!='jpg':
+        im = Image.open(file)
+        rgb_im = im.convert('RGB')
+        rgb_im.save('/home/madhur/BDP-Police-FIR-Project/web-app/uploads/car.jpg')
+    else:
+        name[0]='car'
+        name=name[0]+'.'+name[1]
+        im = Image.open(file)
+        im.save('/home/madhur/BDP-Police-FIR-Project/web-app/uploads/car.jpg')
+    
+    fd = open('/home/madhur/BDP-Police-FIR-Project/data.csv', 'r')
+    df = pd.read_csv(fd)
+    # f = os.path.join(app.config['UPLOAD_FOLDER'], 'car.jpg')
+    # file.save(f)
+    IMAGE_PATH = '/home/madhur/BDP-Police-FIR-Project/web-app/uploads/car.jpg'
+    SECRET_KEY = 'sk_9791a1be475cae14e87a2abf'
+    with open(IMAGE_PATH, 'rb') as image_file:
+        img_base64 = base64.b64encode(image_file.read())
+    url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=in&secret_key=%s' % (SECRET_KEY)
+    r = requests.post(url, data = img_base64)
+    number=r.json()['results'][0]['plate'] if r.json()['results'][0]['plate'] else 'Wrong Image'
+    color= r.json()['results'][0]['vehicle']['color'][0]['name'] if r.json()['results'][0]['vehicle']['color'][0]['name'] else 'Wrong Image'
+    make_company=r.json()['results'][0]['vehicle']['make'][0]['name'] if r.json()['results'][0]['vehicle']['make'][0]['name'] else 'Wrong Image'
+    body_type=r.json()['results'][0]['vehicle']['body_type'][0]['name'] if r.json()['results'][0]['vehicle']['body_type'][0]['name'] else 'Wrong Image'
+    year=r.json()['results'][0]['vehicle']['year'][0]['name'] if r.json()['results'][0]['vehicle']['year'][0]['name'] else 'Wrong Image'
+    car_name=r.json()['results'][0]['vehicle']['make_model'][0]['name'] if r.json()['results'][0]['vehicle']['make_model'][0]['name'] else 'Wrong Image'
+    status='Not stolen'
+    for i in range(len(df)):
+        if df['Registration Number'][i]==number:
+            status=df['STATUS'][i]
+    value=[status, number, color, make_company, body_type, year, car_name]
+    return render_template('image.html', title='Uploaded Image Analysis', values=value)
